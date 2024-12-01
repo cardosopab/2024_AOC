40a8bf9f04f956a68bf154de010505fa171fd5f9
Pablo <cardosopab@gmail.com>
2024-12-01 12:15:19 -0600

    Add part 2

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -4,17 +4,17 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-int main() {
+void part_1(std::string file_name) {
   // Add puzzle.txt to directory
-  std::string file_name = "puzzle.txt";
   std::fstream input_file(file_name);
-  std::string curr;
+  std::string curr_line;
   std::vector<int> arr1, arr2;
 
-  while (std::getline(input_file, curr)) {
-    std::istringstream line_stream(curr);
+  while (std::getline(input_file, curr_line)) {
+    std::istringstream line_stream(curr_line);
     int col1, col2;
     if (line_stream >> col1 >> col2) {
       arr1.push_back(col1);
@@ -30,5 +30,35 @@ int main() {
   for (int i = 0; i < arr1.size(); i++) {
     res += abs(arr1[i] - arr2[i]);
   }
-  std::cout << res << std::endl;
+  std::cout << "Part 1: " << res << std::endl;
+}
+
+void part_2(std::string file_name) {
+  // Add puzzle.txt to directory
+  std::fstream input_file(file_name);
+  std::string curr_line;
+  std::vector<int> arr1;
+  std::unordered_map<int, int> map;
+
+  while (std::getline(input_file, curr_line)) {
+    std::istringstream line_stream(curr_line);
+    int col1, col2;
+    if (line_stream >> col1 >> col2) {
+      arr1.push_back(col1);
+      map[col2]++;
+    }
+  }
+  input_file.close();
+
+  int res = 0;
+  for (int n : arr1) {
+    res += n * map[n];
+  }
+  std::cout << "Part 2: " << res << std::endl;
+}
+int main() {
+  // std::string file_name = "test.txt";
+  std::string file_name = "puzzle.txt";
+  part_1(file_name);
+  part_2(file_name);
 }
