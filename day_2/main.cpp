ff48eabb0a4493d9b77a1fd79203f3447ae8f043
Pablo <cardosopab@gmail.com>
2024-12-02 17:31:20 -0600

    part 2

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -47,8 +48,72 @@ void part_1(std::string file_name) {
   std::cout << "Part 1: " << res << std::endl;
 }
 
+bool is_safe(std::vector<int> arr) {
+  bool increasing = arr[0] < arr[1];
+  bool monotonic = true;
+
+  if (increasing) {
+    for (int i = 1; i < arr.size(); i++) {
+      if (arr[i - 1] >= arr[i] || abs(arr[i - 1] - arr[i]) > 3) {
+        monotonic = false;
+        break;
+      }
+    }
+  } else {
+    for (int i = 1; i < arr.size(); i++) {
+      if (arr[i - 1] <= arr[i] || abs(arr[i - 1] - arr[i]) > 3) {
+        monotonic = false;
+        break;
+      }
+    }
+  }
+
+  return monotonic;
+}
+
+void part_2(std::string file_name) {
+  std::fstream input_file(file_name);
+  std::string curr_line;
+  int res = 0;
+
+  while (std::getline(input_file, curr_line)) {
+    std::istringstream line_stream(curr_line);
+    int val;
+    std::vector<int> arr;
+
+    while (line_stream >> val) {
+      arr.push_back(val);
+    }
+
+    bool safe = false;
+    for (int i = 0; i < arr.size(); i++) {
+      std::vector<int> temp;
+      temp.reserve(arr.size() - 1);
+      for (int j = 0; j < arr.size(); j++) {
+        if (i != j) {
+          temp.push_back(arr[j]);
+        }
+      }
+
+      if (is_safe(temp)) {
+        safe = true;
+        break;
+      }
+    }
+
+    if (safe)
+      res++;
+  }
+  input_file.close();
+
+  std::cout << "Part 2: " << res << std::endl;
+}
+
 int main() {
-  std::string file_name = "test.txt";
-  // std::string file_name = "puzzle.txt";
-  part_1(file_name);
+  // std::string file_name = "test.txt";
+  std::string file_name = "puzzle.txt";
+  // part_1(file_name);
+  // First 495 too low.
+  // Second 501 too low.
+  part_2(file_name);
 }
